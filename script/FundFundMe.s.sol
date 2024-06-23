a851b79ca5bf783c57562f07f57075ded75992c5
Mohamed Said <mohamed.said.fci@gmail.com>
2024-06-23 17:29:58 +0200

    added FundFundMe script

---
+++ script/FundFundMe.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract FundFundMe is Script {
+    function run() external {
+
+    }
+}
